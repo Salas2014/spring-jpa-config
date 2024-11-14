63458b2b8a4df4b914d9843478e6de9073c77a9b
Vladyslav Hasporian <saslasengland33@gmail.com>
2024-11-14 20:29:33 +0200

    config without xml, add hikari pool, propertySource, add provider and pakageToScan for entity manager

--- src/main/java/org/example/config/RootConfig.java
+++ src/main/java/org/example/config/RootConfig.java
@@ -1,30 +1,56 @@
 package org.example.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.TransactionManager;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan("org.example")
 @EnableJpaRepositories("org.example.repository")
+@PropertySource("classpath:application.properties")
 public class RootConfig {
+
+    @Value("${spring.datasourse.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasourse.username}")
+    private String dataSourceUserName;
+
+    @Value("${spring.datasourse.password}")
+    private String dataSourcePassword;
 
 
     @Bean("entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setPersistenceUnitName("default");
-        emf.setPackagesToScan("com.example");
+        emf.setDataSource(dataSource());
+        emf.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emf.setPackagesToScan("org.example.entity");
         return emf;
     }
 
     @Bean
     public TransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUserName);
+        dataSource.setPassword(dataSourcePassword);
+        return dataSource;
     }
 }
